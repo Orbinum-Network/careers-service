30471097efc1b3dc7d76c42b5953dcfaa8d3af4e
keepliam <kqoepfksal1250gk2@habenwir.com>
2025-02-19 18:59:25 -0300

    Add CareerModel with CRUD operations using SQLite

---
+++ src/models/careers.js
@@ -0,0 +1,96 @@
+const db = require('../db/db');
+
+class CareerModel {
+    static getAll() {
+        return new Promise((resolve, reject) => {
+            db.all("SELECT * FROM careers", [], (err, rows) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(rows);
+                }
+            });
+        });
+    }
+
+    static getById(id) {
+        return new Promise((resolve, reject) => {
+            const sql = `SELECT * FROM careers WHERE id = ?`;
+            const values = [id];
+
+            db.get(sql, values, (err, row) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(row);
+                }
+            })
+        })
+    }
+
+    static create(careerData) {
+        return new Promise((resolve, reject) => {
+            const { id, title, location, employment_type, department, description, state } = careerData;
+
+            const sql = `
+                INSERT INTO careers (id, title, location, employment_type, department, description, state) 
+                VALUES (?, ?, ?, ?, ?, ?, ?)
+            `;
+            const values = [id, title, location, employment_type, department, description, state];
+
+            db.run(sql, values, function (err) {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve({ id, title, location, employment_type, department, description, state });
+                }
+            });
+        });
+    }
+
+    static update(careerData) {
+        return new Promise((resolve, reject) => {
+            const { id, title, location, employment_type, department, description, state } = careerData;
+
+            const sql = `
+                UPDATE careers
+                SET title = ?, location = ?, employment_type = ?, department = ?, description = ?, state = ?, updated_at = CURRENT_TIMESTAMP
+                WHERE id = ?;
+            `;
+            const values = [title, location, employment_type, department, description, state, id];
+
+            db.run(sql, values, function (err) {
+                if (err) {
+                    reject(err);
+                } else {
+                    if (this.changes > 0) {
+                        resolve({ id, ...careerData });
+                    } else {
+                        resolve(null);
+                    }
+                }
+            });
+        });
+    }
+
+    static delete(id) {
+        return new Promise((resolve, reject) => {
+            const sql = `DELETE FROM careers WHERE id =?`;
+            const values = [id];
+
+            db.run(sql, values, function (err) {
+                if (err) {
+                    reject(err);
+                } else {
+                    if (this.changes > 0) {
+                        resolve();
+                    } else {
+                        resolve(null);
+                    }
+                }
+            });
+        });
+    }
+}
+
+module.exports = CareerModel;
