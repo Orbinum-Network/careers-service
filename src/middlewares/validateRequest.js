5c3221f7f2d82da576d38c6eb9d405cceb5eef93
keepliam <kqoepfksal1250gk2@habenwir.com>
2025-02-19 18:59:01 -0300

    Add request validation middleware with CORS origin check and logging

---
+++ src/middlewares/validateRequest.js
@@ -0,0 +1,25 @@
+const { CORS_ORIGINS } = require('../config/envs');
+const logEntry = require('../utils/logEntry');
+
+const allowedOrigins = (CORS_ORIGINS || '').split(',').map(origin => origin.trim());
+
+const validateRequest = (req, res, next) => {
+    const origin = req.headers.origin;
+    const ip = req.ip || req.connection.remoteAddress;
+    
+    if (origin) {
+        if (allowedOrigins.includes(origin)) {
+            next();
+        } else {
+            logEntry(`Invalid Origin: ${origin}, IP: ${ip}`);
+            res.status(403).send('Forbidden');
+        }
+    } else if (ip === '127.0.0.1' || ip === '::1' || ip === '::ffff:127.0.0.1') {
+        next();
+    } else {
+        logEntry(`Not Local: IP: ${ip}`);
+        res.status(403).send('Forbidden');
+    }
+};
+
+module.exports = validateRequest;
