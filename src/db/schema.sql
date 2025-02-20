46df48f3f4b0e84fbfc0e61c4560edd14e15e593
keepliam <kqoepfksal1250gk2@habenwir.com>
2025-02-20 17:43:31 -0300

    Changed employment_type checks

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS careers (
     id TEXT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     location VARCHAR(255) NOT NULL,
-    employment_type TEXT CHECK(employment_type in ('FULL TIME', 'PART TIME', 'FREELANCE')),
+    employment_type TEXT CHECK(employment_type in ('Full Time', 'Part Time', 'Freelance')),
     department VARCHAR(255) NOT NULL,
     description TEXT NOT NULL,
     state TEXT CHECK(state in ('FINISHED', 'CANCELLED', 'ACTIVE')),
