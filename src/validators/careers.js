a9c089de6815179a21d3065ee033a84116b91cd3
keepliam <kqoepfksal1250gk2@habenwir.com>
2025-02-20 17:48:06 -0300

    rename employment_type validations

--- src/validators/careers.js
+++ src/validators/careers.js
@@ -4,8 +4,8 @@ const careerValidationRules = [
     body("title").notEmpty().withMessage("El título es obligatorio"),
     body("location").notEmpty().withMessage("La ubicación es obligatoria"),
     body("employment_type")
-        .isIn(["FULL TIME", "PART TIME", "FREELANCE"])
-        .withMessage("El tipo de empleo debe ser FULL TIME, PART TIME o FREELANCE"),
+        .isIn(["Full Time", "Part Time", "Freelance"])
+        .withMessage("El tipo de empleo debe ser Full Time, Part Time o Freelance"),
     body("department").notEmpty().withMessage("El departamento es obligatorio"),
     body("description").notEmpty().withMessage("La descripción es obligatoria"),
     body("state")
