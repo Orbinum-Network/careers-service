4622b34e27fb560eec2e96c39e8bba74b0f3f9ea
keepliam <kqoepfksal1250gk2@habenwir.com>
2025-02-19 23:20:24 -0300

    Add CORS origin validation with Docker IP and client IP logging in validateRequest middleware

--- src/middlewares/validateRequest.js
+++ src/middlewares/validateRequest.js
@@ -5,19 +5,24 @@ const allowedOrigins = (CORS_ORIGINS || '').split(',').map(origin => origin.trim
 
 const validateRequest = (req, res, next) => {
     const origin = req.headers.origin;
-    const ip = req.ip || req.connection.remoteAddress;
-    
+
+    // Get the client's IP address; this is the IP assigned to the container in Docker.
+    const dockerIp = req.ip || req.connection.remoteAddress;
+
+    // Get the 'X-Forwarded-For' header, which contains the real client IP and the Cloudflare proxy IP.
+    const clientIp = req.headers['x-forwarded-for'];
+
     if (origin) {
         if (allowedOrigins.includes(origin)) {
             next();
         } else {
-            logEntry(`Invalid Origin: ${origin}, IP: ${ip}`);
+            logEntry(`Invalid Origin: ${origin}, DOCKERIZED IP: ${dockerIp}, CLIENT IP: ${clientIp}`);
             res.status(403).send('Forbidden');
         }
     } else if (ip === '127.0.0.1' || ip === '::1' || ip === '::ffff:127.0.0.1' || ip === HOST_IP) {
         next();
     } else {
-        logEntry(`Not Local: IP: ${ip}`);
+        logEntry(`Not Local: DOCKERIZED IP: ${dockerIp}, CLIENT IP: ${clientIp}`);
         res.status(403).send('Forbidden');
     }
 };
