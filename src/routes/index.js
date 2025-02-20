371c2c49f1fcf182638eb6822fa24a821c29473d
keepliam <kqoepfksal1250gk2@habenwir.com>
2025-02-20 17:44:48 -0300

    added apply route with multer for temp files

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const multer = require("multer");
 const router = express.Router();
 const careerController = require('../controllers/careers');
 const careerValidationRules = require('../validators/careers');
+const applicationController = require('../controllers/application');
+
+const upload = multer();
 
 router.get('/', careerController.getAllCareers);
 router.get('/:id', careerController.getCareerById);
 router.post('/', careerValidationRules, careerController.createCareer);
 router.put('/:id', careerValidationRules, careerController.updateCareer);
 router.delete('/:id', careerController.deleteCareer);
+router.post('/apply', upload.single("resume"), applicationController.receiveApplication);
 
 module.exports = router;
