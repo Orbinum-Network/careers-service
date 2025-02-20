374c35e3f1f9a045186a85067f00c1a3cc6981bc
keepliam <kqoepfksal1250gk2@habenwir.com>
2025-02-20 17:41:45 -0300

    aadded email env

--- src/config/envs.js
+++ src/config/envs.js
@@ -1,5 +1,9 @@
 module.exports = {
     PORT: process.env.PORT || 3000,
     CORS_ORIGINS: process.env.CORS_ORIGINS,
-    HOST_IP: process.env.HOST_IP
+    HOST_IP: process.env.HOST_IP,
+    EMAIL_USER: process.env.EMAIL_USER,
+    EMAIL_PASS: process.env.EMAIL_PASS,
+    EMAIL_HOST: process.env.EMAIL_HOST,
+    EMAIL_PORT: process.env.EMAIL_PORT,
 }
