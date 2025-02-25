b4a7f6191430c31d7491ba0c1f64ea084b2a4338
keepliam <kqoepfksal1250gk2@habenwir.com>
2025-02-25 09:29:00 -0300

    added getAll + getByUserIp

--- src/models/applications.js
+++ src/models/applications.js
@@ -1,6 +1,33 @@
 const db = require('../db/db');
 
 class ApplicationModel {
+    static getAll() {
+        return new Promise((resolve, reject) => {
+            db.all("SELECT career_id, user_ip, application_date FROM applications", [], (err, rows) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(rows);
+                }
+            });
+        });
+    }
+
+    static getByUserIp(user_ip, career_id) {
+        return new Promise((resolve, reject) => {
+            const sql = `SELECT career_id, user_ip, application_date FROM applications WHERE user_ip = ? AND career_id = ?`;
+            const values = [user_ip, career_id];
+
+            db.get(sql, values, (err, row) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(row);
+                }
+            });
+        });
+    }
+
     static create(clientIpAddress, career_id) {
         return new Promise((resolve, reject) => {
             const sql = `
