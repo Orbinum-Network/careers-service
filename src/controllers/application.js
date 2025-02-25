ae9dad004cdc0698ca2272c70676b74f0c1b2768
keepliam <kqoepfksal1250gk2@habenwir.com>
2025-02-25 01:21:39 -0300

    added registerClientApplication service + career_id from body + clientIpAddress from middleware

--- src/controllers/application.js
+++ src/controllers/application.js
@@ -1,5 +1,5 @@
 const { validationResult } = require("express-validator");
-const { sendApplicationEmail } = require("../services/application.js");
+const { sendApplicationEmail, registerClientApplication } = require("../services/application.js");
 
 const receiveApplication = async (req, res) => {
     try {
@@ -8,9 +8,11 @@ const receiveApplication = async (req, res) => {
             return res.status(400).json({ errors: errors.array() });
         }
 
-        const { name, email, linkedIn, website, github } = req.body;
+        const { career_id, name, email, linkedIn, website, github } = req.body;
         const resume = req.file;
+        const clientIpAddress = req.clientIpAddress; 
 
+        await registerClientApplication(clientIpAddress, career_id);
         const response = await sendApplicationEmail({ name, email, linkedIn, website, github, resume });
         if (response.success) {
             return res.status(200).json({
