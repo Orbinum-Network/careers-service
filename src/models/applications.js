cf49c1626f13fa2552f3d49423dd612bf8d88d22
keepliam <kqoepfksal1250gk2@habenwir.com>
2025-02-25 01:22:23 -0300

    added model for Application

---
+++ src/models/applications.js
@@ -0,0 +1,23 @@
+const db = require('../db/db');
+
+class ApplicationModel {
+    static create(clientIpAddress, career_id) {
+        return new Promise((resolve, reject) => {
+            const sql = `
+                INSERT INTO applications (career_id, user_ip) 
+                VALUES (?, ?)
+            `;
+            const values = [career_id, clientIpAddress];
+
+            db.run(sql, values, function (err) {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve({ career_id, user_ip });
+                }
+            });
+        })
+    }
+}
+
+module.exports = ApplicationModel;
